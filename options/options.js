3ffc860a2eecb9d44dfcbfd7e8d2b71dd56b7408
Crowdbotics <team@crowdbotics.com>
2024-08-16 01:05:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "superx-49075",
+  url: "https://superx-49075.botics.co",
+  api: "https://superx-49075.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
